50beb435d2e0027693aca3e50f84c173737abf70
Rémi Lerolle <lerolle.remi@gmail.com>
2024-08-06 20:36:41 +0200

    useEffect React hook to retrieve CV XML data

--- src/ROUTES/cv/Cv.js
+++ src/ROUTES/cv/Cv.js
@@ -1,4 +1,4 @@
-import {React, useContext} from 'react';
+import {React, useContext, useEffect, useState} from 'react';
 import XMLdataFr from "../../XMLDATA/cv-rl-fr.xml";
 import XMLdataEn from "../../XMLDATA/cv-rl-en.xml";
 import CvArticle from './CvArticle';
@@ -7,64 +7,51 @@ import {LangContext} from '../../App.js'
 
 export default function Cv(){
   const lang = useContext(LangContext);
-  console.log(LangContext)
-  console.log(lang)
+  const [xmlData, setXMLData] = useState(null)
 
-  const prom = getXML( lang === "fr"  
-                          ? XMLdataFr
-                          : lang === "en"
-                            ? XMLdataEn
-                            : null  ).then(
-    xmlResponse => {
-      return (<>
-        <p>lang</p>
-      <CvArticle xmlResponse={ xmlResponse } />
-      </>
-    )
-    },
-    responseStatusText => {
-      console.log(responseStatusText);
+  useEffect( () => {
+    const x = lang === "fr"
+      ? XMLdataFr
+      : lang === "en"
+        ? XMLdataEn
+        : null
+    
+    if ( x ){
+      getXML( x )
+        .then( response => {
+          setXMLData(response);
+        },
+        response => {
+          console.log("case reject") 
+          console.log(response) }
+      )
     }
+  }, [lang])
+
+  return (
+    xmlData 
+      ? <CvArticle xmlResponse={ xmlData } /> 
+      : null
   )
-
-  
-
-  // return(
-  //   <>
-  //   <p>{lang}</p>
-  //   <CvArticle xmlResponse={ this.state.xmlResponse } />
-  //   </>
-  // )
 }
 
-
-// function CvFromXML(XML){
-
-  
-//   render (){
-//       return(<CvArticle xmlResponse={ this.state.xmlResponse } />)
-//     } 
-// //   }
-
 function getXML( XMLdata ) {
-  console.log("in getXML ")
-  console.log("XMLdata")
-  console.log(XMLdata)
 
-  return new Promise((resolve, reject) => {
+  if ( XMLdata ){
+    return new Promise((resolve, reject) => {
 
-    const req = new XMLHttpRequest();
+      const req = new XMLHttpRequest();
 
-    req.open( "GET", XMLdataFr );
-    req.onload = () => {
-      if ( req.status >= 200 && req.status < 300){
-        resolve( req.responseXML );
-      }else{
-        reject( req.statusText );
+      req.open( "GET", XMLdata );
+      req.onload = () => {
+        if ( req.status >= 200 && req.status < 300){
+          resolve( req.responseXML );
+        }else{
+          reject( req.statusText );
+        }
       }
-    }
-    req.onerror = () => reject(req.statusText);
-    req.send();
-
-  })
+      req.onerror = () => reject(req.statusText);
+      req.send();
+    })
+  }
 }
