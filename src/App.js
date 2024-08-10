021abca0f157464e403d7469fdc99892d0ceb9de
Rémi Lerolle <lerolle.remi@gmail.com>
2024-08-10 10:07:04 +0200

    addtion of formalizz and portfolio/cv components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import { createContext, useState } from 'react';
 import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, ButtonGroup, Container, Navbar } from 'react-bootstrap';
+import { Button, ButtonGroup, Container, Navbar, NavDropdown } from 'react-bootstrap';
 
 import i18n from './Translation/i18n';
 import Cv from './ROUTES/cv/Cv';
 import PortfolioHome from './ROUTES/PortfolioHome';
 import Home from './ROUTES/Home';
+import Formalizz from './ROUTES/portfolio/Formalizz';
 
 export const LangContext = createContext(null);
 
@@ -34,9 +35,20 @@ function App() {
             <Link to="/cv" className="list" >
               {i18n.t('Resume')}
             </Link>
-            <Link to="/portfolio-home" className="list" >
-              Portfolio
-            </Link>
+            <NavDropdown title="Portfolio" id="basic-nav-dropdown">
+              <Link to="/portfolio-home" className="list" >
+                Portfolio
+              </Link>
+              <NavDropdown.Item href="#action/3.1">
+              </NavDropdown.Item>
+                <Link to="/portfolio/Formalizz" className="list" >
+                  Formalizz
+                </Link>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            </NavDropdown>
           </Navbar.Collapse>
 
           <ButtonGroup className='mb-2 me-5'>
@@ -53,6 +65,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/cv" element={<Cv/>} />
           <Route path="/portfolio-home" element={<PortfolioHome />} />
+          <Route path="/portfolio/Formalizz" element={<Formalizz />} />
         </Routes>
 
       </Container>
---
+++ src/ROUTES/portfolio/Formalizz.js
@@ -0,0 +1,7 @@
+import {React, useContext} from 'react';
+
+export default function Formalizz(){
+  return(
+    <h1>Formalizz</h1>
+  )
+}
