98cd5fec85c6f6d5733b318f3b8973b36ed1e973
Rémi Lerolle <lerolle.remi@gmail.com>
2024-08-22 21:06:08 +0200

    Addition of image for Portfolio project

--- src/ROUTES/portfolio/PortfolioCV.js
+++ src/ROUTES/portfolio/PortfolioCV.js
@@ -1,6 +1,7 @@
 import { React, useContext } from 'react';
 import { ProjectDataClass } from "../../Classes/ProjectDataClass.tsx"
 import { Project } from './Project.js';
+import Logo_Portfolio from "../../IMG/Logo_Portfolio.png"
 
 export default function PortfolioCV() {
   return (
@@ -13,5 +14,6 @@ export const PortfolioCVData = new ProjectDataClass(
     "title": "Pages Perso",
     "technoList": ["React", "CSS", "Bootstrap"],
     "projectPath": "/portfolio/PortfolioCV",
-    "description": "projet.portfolio.description"
+    "description": "projet.portfolio.description",
+    "imgUrl": Logo_Portfolio
   })
