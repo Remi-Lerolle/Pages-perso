1dd5f2c9472335f2b4a0605ad6b11505532aea49
Rémi Lerolle <lerolle.remi@gmail.com>
2024-08-25 10:35:47 +0200

    XSL studio project has an image & Project technoListToBagde function added

--- src/ROUTES/portfolio/Project.js
+++ src/ROUTES/portfolio/Project.js
@@ -1,55 +1,62 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { Badge, Button, Card, Container, Stack, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import i18n from '../../Translation/i18n';
-import {LangContext} from '../../App.js'
+import { LangContext } from '../../App.js'
 
 
-export function Project({data}) {
-  const lang = useContext(LangContext);
-  i18n.changeLanguage(lang);
+export function Project({ data }) {
+	const lang = useContext(LangContext);
+	i18n.changeLanguage(lang);
 
-  return (<>
-    <Container>
-      <h1>{data.title}</h1>
-    </Container>
-    <Container>
-      <Image src={data.imgUrl} />
-    </Container>
-    <Container>
-      {i18n.t(data.description)}
-    </Container>
-  </>)
+	return (<>
+		<Container>
+			<h1>{data.title}</h1>
+		</Container>   
+		<Container>
+			{technoListToBadges( data.technoList, data.title )}
+		</Container>
+		<Container>
+			<Image src={data.imgUrl} />
+		</Container>
+		<Container>
+			{i18n.t(data.description)}
+		</Container>
+	</>)
 }
 
 export function ProjectCard({ data }) {
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Header>
-        <Card.Title>{data.title}</Card.Title>
-      </Card.Header>
-      <Card.Img variant="top" src={data.imgUrl} />
-      <Card.Body>
-        <Stack direction="horizontal" gap={2}>
-          {data.technoList.map(
-            t => {
-              return (
-                <React.Fragment key={`${t}-${data.title}-techno-bagde`}>
-                  <Badge bg="info" className="mr-2">{t}</Badge>
-                </React.Fragment>
-              )
-            })}
-            </Stack>
-        <Button className="bg-info border-0 mt-2">
-          <Link
-            className='text-white text-decoration-none'            
-            to={data.projectPath} 
-            >
-            {data.title}
-          </Link>
-        </Button>
-      </Card.Body>
-    </Card>
+	return (
+		<Card style={{ width: '18rem' }}>
+			<Card.Header>
+				<Card.Title>{data.title}</Card.Title>
+			</Card.Header>
+			<Card.Img variant="top" src={data.imgUrl} />
+			<Card.Body>
+				{technoListToBadges(data.technoList, data.title)}
+				<Button className="bg-info border-0 mt-2">
+					<Link
+						className='text-white text-decoration-none'
+						to={data.projectPath}
+					>
+						{data.title}
+					</Link>
+				</Button>
+			</Card.Body>
+		</Card>
 
-  )
+	)
+}
+
+function technoListToBadges(technoList, title) {
+	return (<Stack direction="horizontal" gap={2}>
+		{technoList.map(
+			t => {
+				return (
+					<React.Fragment key={`${t}-${title}-techno-bagde`}>
+						<Badge bg="info" className="mr-2">{t}</Badge>
+					</React.Fragment>
+				)
+			})}
+	</Stack>)
 }
--- src/ROUTES/portfolio/StudioXSL.js
+++ src/ROUTES/portfolio/StudioXSL.js
@@ -1,6 +1,7 @@
 import {React, useContext} from 'react';
 import { ProjectDataClass } from "../../Classes/ProjectDataClass.tsx"
 import { Project } from './Project.js';
+import xmlIcon from "../../IMG/xml-icon.png"
 
 export default function StudioXSL(){
   return(
@@ -12,6 +13,7 @@ export const StudioXSLDataClass = new ProjectDataClass(
   { "title": "Studio XSL",
   "technoList":[ "React", "CSS", "Bootstrap", "XSL", "API"],
   "projectPath": "/portfolio/StudioXSL",
-  "description": "projet.studioXSL.description"
+  "description": "projet.studioXSL.description",
+  "imgUrl": xmlIcon
 }
 )
