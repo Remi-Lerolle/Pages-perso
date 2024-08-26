85dcfe896e48be9fee30c04bb10aa3c74aebd266
Rémi Lerolle <lerolle.remi@gmail.com>
2024-08-26 20:47:30 +0200

    Navbar now collapse on mobile screen

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 import { createContext, useState } from 'react';
 import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, ButtonGroup, Container, Navbar, NavDropdown } from 'react-bootstrap';
+import { Button, ButtonGroup, Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 import i18n from './Translation/i18n';
 import Cv from './ROUTES/cv/Cv';
 import PortfolioHome from './ROUTES/portfolio/PortfolioHome';
 import Home from './ROUTES/Home';
-import {Formalizz} from './ROUTES/portfolio/Formalizz';
+import { Formalizz } from './ROUTES/portfolio/Formalizz';
 import PortfolioCV from './ROUTES/portfolio/PortfolioCV';
 import StudioXSL from './ROUTES/portfolio/StudioXSL';
 
@@ -17,92 +17,88 @@ export const LangContext = createContext(null);
 
 function App() {
 
-  const location = useLocation();
+	const location = useLocation();
 
-  const [lang, setLangState] = useState("fr");
-  i18n.changeLanguage(lang)
+	const [lang, setLangState] = useState("fr");
+	i18n.changeLanguage(lang)
 
-  function handleClickLang(langValue) {
-    setLangState(langValue);
-  }
+	function handleClickLang(langValue) {
+		setLangState(langValue);
+	}
 
-  return (
-    <LangContext.Provider value={lang}>
+	return (
+		<LangContext.Provider value={lang}>
 
-{ /*
-    TO DO:
-    the nav bar is not responsive
-*/}
-      <Container fluid className='p-0'>
-        <Navbar>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Link to="/home" className="list" >
-              {i18n.t('Home')}
-            </Link>
-            <Link to="/cv" className="list" >
-              {i18n.t('Resume')}
-            </Link>
+			<Navbar expand="lg">
 
-            <NavDropdown 
-              title="Portfolio"
-              className='navBar-dropdown'>
-              <NavDropdown.Item
-                as={Link}
-                className='App-link'
-                to="/portfolio-home">
-                Portfolio Home
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item
-                as={Link}
-                className='App-link'
-                to="/portfolio/PortfolioCV">
-                PortfolioCV
-              </NavDropdown.Item>
-              <NavDropdown.Item  
-                className='App-link'
-                as={Link}
-                to="/portfolio/Formalizz">
-                Formalizz
-              </NavDropdown.Item>
-              <NavDropdown.Item  
-                className='App-link'
-                as={Link}
-                to="/portfolio/StudioXSL">
-                Studio XSL
-              </NavDropdown.Item>
-            </NavDropdown>
+				<Container>
+					<Navbar.Toggle />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav>
+							<Link to="/home" className="list" >
+								{i18n.t('Home')}
+							</Link>
+							<Link to="/cv" className="list" >
+								{i18n.t('Resume')}
+							</Link>
 
-          </Navbar.Collapse>
+							<NavDropdown
+								title="Portfolio"
+								className='navBar-dropdown'
+								style={{ paddingTop: "0.5rem", paddingLeft: "1rem" }}>
+								<NavDropdown.Item
+									as={Link}
+									className='App-link'
+									to="/portfolio-home">
+									Portfolio Home
+								</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item
+									as={Link}
+									className='App-link'
+									to="/portfolio/PortfolioCV">
+									PortfolioCV
+								</NavDropdown.Item>
+								<NavDropdown.Item
+									className='App-link'
+									as={Link}
+									to="/portfolio/Formalizz">
+									Formalizz
+								</NavDropdown.Item>
+								<NavDropdown.Item
+									className='App-link'
+									as={Link}
+									to="/portfolio/StudioXSL">
+									Studio XSL
+								</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+					</Navbar.Collapse>
 
-          <ButtonGroup className='mb-2 me-5'>
-            <Button
-              variant={lang === "fr" ? "primary" : "secondary"}
-              onClick={() => handleClickLang("fr")}>fr</Button>
-            <Button
-              variant={lang === "en" ? "primary" : "secondary"}
-              onClick={() => handleClickLang("en")} >en</Button>
-          </ButtonGroup>
-        </Navbar>
+					<ButtonGroup className='mb-2 me-5 me-2'>
+						<Button
+							variant={lang === "fr" ? "primary" : "secondary"}
+							onClick={() => handleClickLang("fr")}>fr</Button>
+						<Button
+							variant={lang === "en" ? "primary" : "secondary"}
+							onClick={() => handleClickLang("en")} >en</Button>
+					</ButtonGroup>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/cv" element={<Cv />} />
-          <Route path="/portfolio-home" element={<PortfolioHome />} />
-          <Route path="/portfolio/Formalizz" element={<Formalizz />} />
-          <Route path="/portfolio/PortfolioCV" element={<PortfolioCV />} />
-          <Route path="/portfolio/StudioXSL" element={<StudioXSL />} />
-        </Routes>
+				</Container>
+			</Navbar>
 
-      </Container>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/cv" element={<Cv />} />
+				<Route path="/portfolio-home" element={<PortfolioHome />} />
+				<Route path="/portfolio/Formalizz" element={<Formalizz />} />
+				<Route path="/portfolio/PortfolioCV" element={<PortfolioCV />} />
+				<Route path="/portfolio/StudioXSL" element={<StudioXSL />} />
+			</Routes>
 
-      {location.pathname === "/CV-V2"
-        ? <Home />
-        : ""
-      }
-    </LangContext.Provider>
-  );
+		</LangContext.Provider >
+	);
 }
 
 export default App;
--- src/ROUTES/cv/Cv.js
+++ src/ROUTES/cv/Cv.js
@@ -1,61 +1,71 @@
-import {React, useContext, useEffect, useState} from 'react';
+import { React, useContext, useEffect, useState } from 'react';
 import XMLdataFr from "../../XMLDATA/cv-rl-fr.xml";
 import XMLdataEn from "../../XMLDATA/cv-rl-en.xml";
 import { Container } from 'react-bootstrap';
 import CvArticle from './CvArticle';
-import {LangContext} from '../../App.js'
+import { LangContext } from '../../App.js'
 import i18n from '../../Translation/i18n.js';
 
-export default function Cv(){
-  const lang = useContext(LangContext);
-  const [xmlData, setXMLData] = useState(null)
-  i18n.changeLanguage(lang)
+export default function Cv() {
+	const lang = useContext(LangContext);
+	const [xmlData, setXMLData] = useState(null)
+	i18n.changeLanguage(lang)
 
-  useEffect( () => {
-    const x = lang === "fr"
-      ? XMLdataFr
-      : lang === "en"
-        ? XMLdataEn
-        : null
-    
-    if ( x ){
-      getXML( x )
-        .then( response => {
-          setXMLData(response);
-        },
-        response => {
-          console.log("case reject") 
-          console.log(response) }
-      )
-    }
-  }, [lang])
+	useEffect(() => {
+		const x = lang === "fr"
+			? XMLdataFr
+			: lang === "en"
+				? XMLdataEn
+				: null
 
-  return (
-    <Container className="cvArticle"><h1>{i18n.t("CV.RL")}</h1>
-      { xmlData 
-          ? <CvArticle xmlResponse={ xmlData } /> 
-          : null}
-    </Container>
-  )
+		if (x) {
+			getXML(x)
+				.then(response => {
+					setXMLData(response);
+				},
+					response => {
+						console.log("case reject")
+						console.log(response)
+					}
+				)
+		}
+	}, [lang])
+
+	return (
+		<Container className="cvArticle"><h1>{i18n.t("CV.RL")}</h1>
+			{xmlData
+				? <CvArticle xmlResponse={xmlData} />
+				: null}
+		</Container>
+	)
 }
 
-function getXML( XMLdata ) {
+/*
+		TO DO:
+		LINK TO DOWNLOAD PDF
+*/
+/*
+		TO DO:
+		In mobile screen, "Devel.." card has offset
+*/
 
-  if ( XMLdata ){
-    return new Promise((resolve, reject) => {
+function getXML(XMLdata) {
 
-      const req = new XMLHttpRequest();
+	if (XMLdata) {
+		return new Promise((resolve, reject) => {
 
-      req.open( "GET", XMLdata );
-      req.onload = () => {
-        if ( req.status >= 200 && req.status < 300){
-          resolve( req.responseXML );
-        }else{
-          reject( req.statusText );
-        }
-      }
-      req.onerror = () => reject(req.statusText);
-      req.send();
-    })
-  }
+			const req = new XMLHttpRequest();
+
+			req.open("GET", XMLdata);
+			req.onload = () => {
+				if (req.status >= 200 && req.status < 300) {
+					resolve(req.responseXML);
+				} else {
+					reject(req.statusText);
+				}
+			}
+			req.onerror = () => reject(req.statusText);
+			req.send();
+		})
+	}
 }
