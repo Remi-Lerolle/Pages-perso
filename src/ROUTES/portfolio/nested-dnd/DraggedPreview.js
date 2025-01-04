c2457f2fae7816e89e89ea52a2a2acbdeffeef62
Rémi Lerolle <lerolle.remi@gmail.com>
2025-01-04 18:45:16 +0100

    Rename Preview to DraggedPreview and add it to the component root

--- src/ROUTES/portfolio/nested-dnd/DraggedPreview.js
+++ src/ROUTES/portfolio/nested-dnd/DraggedPreview.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { usePreview } from "react-dnd-preview";
 
-export const Preview = () => {
+export const DraggedPreview = () => {
 	const preview = usePreview({
 		placement: "top", padding: { x: -20, y: 0 }
 	})
+
 	if (!preview.display) { return null }
+	console.log(preview)
 
 	const { itemType, item, style, ref } = preview;
 
-	return <div>
+	return <div style={{ position: "absolute" }}>
 		yo preview
 	</div>
 }
