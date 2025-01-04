b70fcc5b667282cbae1434d93575331b53bec839
Rémi Lerolle <lerolle.remi@gmail.com>
2025-01-04 20:50:36 +0100

    NestedDnd DraggablePreview display children

--- src/ROUTES/portfolio/nested-dnd/DraggedPreview.js
+++ src/ROUTES/portfolio/nested-dnd/DraggedPreview.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { usePreview } from "react-dnd-preview";
 import { ItemTypes } from "./ItemTypes";
+import { useSelector } from "react-redux";
+import { WidgetToDrag } from "./Widget";
 
 export const DraggedPreview = () => {
 	const preview = usePreview({
 		placement: "top", padding: { x: -20, y: 0 }
 	})
 
+	const listOfWidgets = useSelector((state) => state.listOfWidgets.value);
+
 	if (!preview.display) { return null }
-	console.log(preview)
+
+	const thisWidget = listOfWidgets[preview.item.pos]
 
 	const { itemType, item, style, ref } = preview;
 	let computedClassName = "widgetToDrag"
@@ -24,5 +29,13 @@ export const DraggedPreview = () => {
 		ref={ref}
 		style={{ ...style, zIndex: 3 }}>
 		{`${ItemTypes[preview.itemType]} ${preview.item.pos || ""}`}
+		{preview.itemType === "GROUPDROPPED"
+			? thisWidget.children.map(
+				(_, indexInGrid) => <WidgetToDrag
+					widgeType="SIMPLENESTED"
+					index={preview.item.pos}
+					indexInGrid={indexInGrid}
+				/>)
+			: null}
 	</div>
 }
