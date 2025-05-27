6183e5b2eefd74b7860f90ff2d71868d4fa2efe4
Rémi Lerolle <lerolle.remi@gmail.com>
2025-05-27 18:31:15 +0200

    Addition of diagram sequence describing use of API La Poste #19

--- src/ROUTES/portfolio/FormalizLaposteApi.js
+++ src/ROUTES/portfolio/FormalizLaposteApi.js
@@ -3,5 +3,6 @@ import React from "react";
 export function FormalizLaposteApi() {
 	return <div>
 		<p>Les Utlisateurs du site Formalizz au moment où ils génèrent une cession de créance, ont la possibilité de l'associer à un numéro de recommandé.</p>
+		<p>Plus tard lorsque l'utlisateur retourne sur la liste </p>
 	</div>
 }
