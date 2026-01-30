c5a9b3a059cc7911087d39c9bf19fd915e10ccd2
remi <lerolle.remi@gmail.com>
2026-01-30 15:54:13 +0100

    [HOME] Addition of Image and section #24

--- src/ROUTES/Home.js
+++ src/ROUTES/Home.js
@@ -1,17 +1,34 @@
-import {React, useContext} from 'react';
-import { Container } from 'react-bootstrap';
+import { React, useContext } from 'react';
+import { Container, Row } from 'react-bootstrap';
 import i18n from '../Translation/i18n';
-import {LangContext} from '../App.js'
+import { LangContext } from '../App.js'
+import grenouille from '../IMG/grenouille.jpg'
 
 function Home() {
-  const lang = useContext(LangContext);
-  i18n.changeLanguage(lang);
+	const lang = useContext(LangContext);
+	i18n.changeLanguage(lang);
 
-  return (
-    <Container >
-      <h1>{i18n.t('RL.personnal.web.pages')}</h1>
-    </Container>
-  )
+	return <>
+		<Container >
+			<Row>
+				<h1>{i18n.t('RL.personnal.web.pages')}</h1>
+			</Row>
+		</Container>
+
+		<Container 
+			fluid
+			className="ps-0 pe-0" >
+
+			<img
+				style={{ width: '100%'}}
+				src={grenouille} />
+		</Container>
+
+		<Container>		
+			<h2>{i18n.t('Portfolio')}</h2>
+			<h2>{i18n.t('Resume')}</h2>
+		</Container>
+	</>
 }
 
 export default Home;
